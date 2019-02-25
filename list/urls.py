0bf10d5fdf93adaa9890da297c8124aeb9574e9a
namik211091 <namik211091@gmail.com>
2019-02-26 03:46:48 +0400

    second

--- list/urls.py
+++ list/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from . import views
+
+app_name = 'list'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('units/', views.units, name='units'),
-    path('ips/', views.ips, name='ips')
+    #path('ips/', views.ips, name='ips'),
+    path('units/add', views.addUnit, name='addUnit'),
+    url(r'^units/(?P<unit_pk>[0-9]+)/$', views.ips, name='ips'),
+    path('units/createUnit', views.createUnit, name='createUnit')
 
 ]
--- list/views.py
+++ list/views.py
@@ -24,12 +24,41 @@ def units(request) :
 
     return HttpResponse(template.render(context, request))
 
-def ips(request) :
+def ips(request, unit_pk) :
     template = loader.get_template('list/ips.html')
+    current_unit = Unit.objects.get(pk=unit_pk)
+    all_ips = current_unit.ip_set.all()
+    context = {
+        'all_ips': all_ips,
+        'unit_pk': unit_pk
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def addUnit(request) :
+    template = loader.get_template('list/addUnit.html')
     all_ips = Ip.objects.all()
     context = {
         'all_ips': all_ips,
     }
+
+    return HttpResponse(template.render(context, request))
+
+
+def createUnit(request) :
+    template = loader.get_template('list/units.html')
+
+    all_units = Unit.objects.all()
+    context = {
+        'all_units': all_units,
+    }
+
+    if request.POST:
+        vLan = request.POST.get('vLan')
+        name = request.POST.get('name')
+        new_unit = Unit(vLan = vLan, name = name)
+        new_unit.save()
 
     return HttpResponse(template.render(context, request))
 
